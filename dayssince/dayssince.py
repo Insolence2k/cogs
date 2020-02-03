63be5f055827b3e77c5e61d268e7ce7cd4521820
Javad Shafique <javadshafique@hotmail.com>
2020-02-03 09:46:27 +0100

    test

--- dayssince/dayssince.py
+++ dayssince/dayssince.py
@@ -2,6 +2,8 @@ import discord
 from discord.ext import commands
 
 class dayssince:
+    REACTION = "dart"
+
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
@@ -10,9 +12,22 @@ class dayssince:
         """
         mono meltdown
         """
-        for Member in ctx.message.mentions:
-            await self.bot.send_message(ctx.message.channel, str(Member))
+        meltdown_embed = discord.Embed()
+        meltdown_user = discord.Member
+
+        if len(ctx.message.mentions) == 1:
+            meltdown_user = ctx.message.mentions[0]
+        else:
+            meltdown_user = ctx.message.author
         
+        meltdown_message = await self.bot.send_message(ctx.message.channel, embed=meltdown_embed)
+        await self.bot.add_reaction(meltdown_message, self.REACTION)
+
+        while meltdown_message.reactions[0].count:
+            if len(meltdown_message.reactions[0].count) > 1:
+                print(meltdown_message.reactions)
+                break
+
         await self.bot.delete_message(ctx.message)
     
     def storage_get():
