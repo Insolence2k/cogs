d02817a6bacfd1ef715610c59e444977e6e8ff25
Javad Shafique <javadshafique@hotmail.com>
2020-02-03 10:09:04 +0100

    Try a server specific emote

--- dayssince/dayssince.py
+++ dayssince/dayssince.py
@@ -19,7 +19,7 @@ class dayssince:
             meltdown_user = ctx.message.author
         
         meltdown_message = await self.bot.send_message(ctx.message.channel, embed=meltdown_embed)
-        await self.bot.add_reaction(meltdown_message, "beginner")
+        await self.bot.add_reaction(meltdown_message, "ok_4ppl3")
 
         while meltdown_message.reactions[0].count:
             if len(meltdown_message.reactions[0].count) > 1:
