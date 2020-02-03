6e8cb38d646e78bf9d15b697848bea69a71f0853
Javad Shafique <javadshafique@hotmail.com>
2020-02-03 09:53:43 +0100

    try this

--- dayssince/dayssince.py
+++ dayssince/dayssince.py
@@ -19,7 +19,7 @@ class dayssince:
             meltdown_user = ctx.message.author
         
         meltdown_message = await self.bot.send_message(ctx.message.channel, embed=meltdown_embed)
-        await self.bot.add_reaction(meltdown_message, discord.Emoji("dart"))
+        await self.bot.add_reaction(meltdown_message, discord.Emoji(ctx.message.guild.id, "dart"))
 
         while meltdown_message.reactions[0].count:
             if len(meltdown_message.reactions[0].count) > 1:
