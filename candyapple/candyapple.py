5c4001cab2b9e5725fbdc49aa6febdfe5db27e58
Javad Shafique <javadshafique@hotmail.com>
2020-02-05 19:54:48 +0100

    testing paths and data storage w/ Red

--- candyapple/candyapple.py
+++ candyapple/candyapple.py
@@ -1,9 +1,15 @@
 import discord
 from discord.ext import commands
 
+import os
+
 class candyapple:
     def __init__(self, bot: commands.Bot):
         self.bot = bot
+
+    @commands.command(pass_context=True)
+    async def test(self, ctx):
+        await self.bot.say(ctx.message.channel, str(os.path.abspath(os.curdir)))
 
 def setup(bot):
     bot.add_cog(candyapple(bot))
